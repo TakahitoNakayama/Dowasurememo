7cfbedaa128f38573c913428efdd5c661bcb0b3c
Takahito Nakayama <takahito0826@gmail.com>
2021-07-06 07:32:10 +0930

    新規追加の入力欄を追記

--- main/java/com/websarva/wings/android/dowasurememo/AddInputForm.java
+++ main/java/com/websarva/wings/android/dowasurememo/AddInputForm.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.graphics.Color;
 import android.os.Build;
 import android.text.InputType;
+import android.util.DisplayMetrics;
+import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -12,6 +14,7 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.RequiresApi;
 
 public class AddInputForm extends View {
@@ -27,17 +30,17 @@ public class AddInputForm extends View {
      linearLayout=new LinearLayout(getContext());
      linearLayout.setOrientation(LinearLayout.HORIZONTAL);
      linearLayout.setLayoutParams(new LinearLayout.LayoutParams
-             (ViewGroup.LayoutParams.MATCH_PARENT,LinearLayout.LayoutParams.WRAP_CONTENT));
-
-     Button button=new Button(getContext());
-     button.setText("555");
-     TextView textView=new TextView(getContext());
-     textView.setText("あああああ");
-     linearLayout.addView(button);
-     linearLayout.addView(textView);
-
+             (LinearLayout.LayoutParams.MATCH_PARENT,
+                     LinearLayout.LayoutParams.WRAP_CONTENT,
+                     (float) 1));
+     linearLayout.setVerticalGravity(Gravity.CENTER);
 
      ImageView imageView=new ImageView(getContext());
+     imageView.setLayoutParams(new LinearLayout.LayoutParams
+             ((int) convertDp2Px(15,getContext()),
+                     (int) convertDp2Px(15,getContext()),
+                     (float) 0.1));
+     imageView.setLayoutParams(new ViewGroup.MarginLayoutParams());
      imageView.setImageResource(R.drawable.baseline_circle_black_24);
      imageView.setColorFilter(Color.rgb(127,255,212));
      linearLayout.addView(imageView);
@@ -45,17 +48,40 @@ public class AddInputForm extends View {
      EditText editText=new EditText(getContext());
      editText.setTextColor(Color.BLACK);
      editText.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+     editText.setLayoutParams(new LinearLayout.LayoutParams
+             (0,
+                     LinearLayout.LayoutParams.WRAP_CONTENT,
+                     (float) 0.3));
+     linearLayout.addView(editText);
+
+     EditText editText2=new EditText(getContext());
+     editText2.setTextColor(Color.BLACK);
+     editText2.setInputType(InputType.TYPE_NUMBER_FLAG_DECIMAL);
+     editText2.setBackgroundResource(R.drawable.edittext_shape);
+     editText2.setLayoutParams(new LinearLayout.LayoutParams
+                (0,
+                        LinearLayout.LayoutParams.WRAP_CONTENT,
+                        (float) 0.3));
+     linearLayout.addView(editText2);
+
+     TextView textView=new TextView(getContext());
+     textView.setText("aaa");
+     textView.setLayoutParams(new LinearLayout.LayoutParams
+             (0,
+                     LinearLayout.LayoutParams.WRAP_CONTENT,
+                     (float) 0.1));
+     linearLayout.addView(textView);
 
 
 
 
 
     }
-
-
-
-
-
-
+    public static float convertDp2Px(float dp, Context context) {
+        DisplayMetrics metrics = context.getResources().getDisplayMetrics();
+        return dp * metrics.density;
+    }
 
 }
+
+
